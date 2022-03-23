b2582d9afafceb1a3f9b6886cb41e41ef06ed9f7
s.chaikin <s.chaikin@ra-wm.ru>
2022-03-23 11:51:45 +0300

    feat:add footer grid

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,6 +1,18 @@
 import { FooterProps } from "./Footer.props";
-import styles from "./Layout.module.css";
+import styles from "./Footer.module.css";
+import cn from "classnames";
+import { format } from "date-fns";
 
-export const Footer = ({ ...props }: FooterProps): JSX.Element => {
-  return <div {...props}>Footer</div>;
+export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
+  return (
+    <footer className={cn(className, styles.footer)} {...props}>
+      <div>Все права защищены - {format(new Date(), "yyyy")}</div>
+      <a href="#" target="_blank">
+        Пользовательское соглашение
+      </a>
+      <a href="#" target="_blank">
+        Политика конфиденциальности
+      </a>
+    </footer>
+  );
 };
